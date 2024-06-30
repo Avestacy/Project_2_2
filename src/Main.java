038bbf3d870dc8c46de53b5f784efd6d52b7dbbc
Anastasia <avestacy@gmail.com>
2024-06-30 20:26:50 +0300

    add README.md

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    public static void main(String[] args) {
+        int startBalance = 1000; //начальный баланс на счету
+        int addSum = 2000;  //сумма пополнения счета
+
+        int bonus = 0;
+        if (addSum > 1000){
+            bonus = addSum / 100;
+        } else {
+            bonus = 0;
+        }
+        int totalBalans = startBalance + addSum + bonus;
+
+        System.out.print("Бонус за пополнение счета: " + bonus);
+    }
+}
